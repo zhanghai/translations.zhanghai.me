9f0deeaff736d0698f72288eba8ef0cfd92ffe72
Hai Zhang <dreaming.in.code.zh@gmail.com>
2020-06-14 18:46:18 -0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e3f265e1627c262c141892eb7accebb0.js"
+  "/precache-manifest.5089b922456a069a8fca7a861a2d9e09.js"
 );
 
 self.addEventListener('message', (event) => {
