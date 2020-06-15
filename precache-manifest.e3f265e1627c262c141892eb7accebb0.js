0d379a2e8ac2adfd71d6a3a7e7abc3392dcc4f75
Hai Zhang <dreaming.in.code.zh@gmail.com>
2020-06-14 19:55:16 -0700

    Updates

--- precache-manifest.e3f265e1627c262c141892eb7accebb0.js
+++ precache-manifest.e3f265e1627c262c141892eb7accebb0.js
@@ -8,24 +8,24 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/0f25a6291d871a703ea6.worker.js.LICENSE.txt"
   },
   {
-    "revision": "de5716b389f3a5e00aa2de267cfba0de",
+    "revision": "b3a6aa15881a67fa08f0ac0cb0c83c6c",
     "url": "/index.html"
   },
   {
-    "revision": "05ca0b35b5272639699c",
-    "url": "/static/css/main.28e74ae4.chunk.css"
+    "revision": "de83db834695b1572a39",
+    "url": "/static/css/main.af596a94.chunk.css"
   },
   {
-    "revision": "6337e0e64a9a8b337554",
-    "url": "/static/js/2.970e9056.chunk.js"
+    "revision": "b7c21767a07859379dc7",
+    "url": "/static/js/2.aed3902c.chunk.js"
   },
   {
     "revision": "2f925c5c171087a25fbdf59555702a9c",
-    "url": "/static/js/2.970e9056.chunk.js.LICENSE.txt"
+    "url": "/static/js/2.aed3902c.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "05ca0b35b5272639699c",
-    "url": "/static/js/main.242a7946.chunk.js"
+    "revision": "de83db834695b1572a39",
+    "url": "/static/js/main.b36f490d.chunk.js"
   },
   {
     "revision": "07e54bcdf8255233e9d9",
